1a8844bff14c864082ab9ea6ad4dbd620735c38a
Nazib <onlynazib@gmail.com>
2026-08-20 15:46:13 +0600

    fix github action

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// Dynamically set the base path for GitHub Pages based on the repository name.
-// If GITHUB_REPOSITORY is "username/my-repo", this sets base to "/my-repo/".
-const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: repoName,
+  base: '/top-rated-manga/',
   server: { host: '0.0.0.0', port: 3000, allowedHosts: 'all' }
 })
